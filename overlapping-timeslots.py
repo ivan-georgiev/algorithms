840b9061e3a9d5fa4331790abe1e7a449aae5550
ivan.georgiev@outlook.com <ivan.georgiev@outlook.com>
2020-08-26 07:40:11 +0300

    Fix: new algorithm based on endtime sorted

--- overlapping-timeslots.py
+++ overlapping-timeslots.py
@@ -1,6 +1,6 @@
 
 
-def overlaps(st_times: int, end_times: int) -> int:
+def max_timeslots(st_times: int, end_times: int) -> int:
     '''
     Function which receives list of timeslots and output the max number of non-overlapping
 
@@ -9,47 +9,27 @@ def overlaps(st_times: int, end_times: int) -> int:
     Combination st_times[i] and end_times[i] define a timeslot
     Assumed: st_times[i] < end_times[i] and len(st_times) = len(end_times)
 
-    Time complexity is O^2
     '''
-    size = len(st_times)
 
-    # current max set found
-    maxSet = set()
+    # combine timeslot in single array
+    timeslots = [(x,end_times[i]) for i, x in enumerate(st_times)]
+    
+    # sort by endtime, inc
+    timeslots.sort(key=lambda ts: ts[1])
 
-    for i in range(size):
-        # current slot - i
-        istime = st_times[i]
-        iendtime = end_times[i]
-
-        # list of non-overlaping timeslots
-        compatible = {i, }
-
-        # check all timeslots after current timeslot i
-        # range(size) will introduce duplicate sets
-        for j in range(i + 1, size):
-            # next slot - j
-            jstime = st_times[j]
-            jendtime = end_times[j]
-
-            # overlap conditions:
-            # j start/end time is between i(start,end)
-            # i is fully nested in j(start,end)
-            if jendtime > istime and jendtime < iendtime or jstime > istime and jstime < iendtime or jstime <= istime and jendtime >= iendtime:
-                print(f'{i} and {j} overlaps')
-            else:
-                print(f'{i} and {j} do not overlap')
-                compatible.add(j)
-
-        # if currents set is max, update max
-        if len(maxSet) < len(compatible):
-            maxSet = compatible
-
-    print(f'Maximum size set of timeslots: {maxSet}')
-    return len(maxSet)
+    # next timeslot is with endtime and starttime after those of the current
+    cslot = timeslots[0]
+    max = 1
+    for ts in timeslots[1:]:
+        if ts[1] > cslot[1] and ts[0] >= cslot[1]:
+            cslot = ts
+            max = max +1
+    
+    return max
 
 
 if __name__ == '__main__':
-    st_times = [1, 1, 2, 0]
-    end_times = [2, 3, 3, 4]
-    res = overlaps(st_times, end_times)
-    print(f'Result: {res}')
+    st_times = [1, 1, 2, 2, 5]
+    end_times = [2, 3, 3, 3, 6]
+    res = max_timeslots(st_times, end_times)
+    print(res)
